1219a3c034dcc6c26c4d5b76361944e88a57ad35
Joe2323 <joelmellano232323@gmail.com>
2025-12-14 15:35:29 +0100

    Cambio antes de volver

--- gitComands.js
+++ gitComands.js
@@ -17,15 +17,19 @@ cls o clear: limpiar consola
 
 /*comandos GIT
 
+q: salir
 git config: para configurar algo
 git config --global user.name "Joel2323": setear un nombre de usuario a nivel global
 git config --global user.email "joel2323@gmail.com": setear un email a nivel global
 git init: iniciar git en el directorio actual
 git branch -m "cambiar rol": cambiar el nombre de master por otra cosa
 
+git status: muestra lo que falta commitear
 git add "nombre del fichero": prepararlo para commitear
 git commit -m "nombre del fichero": commmitearlo
 git log: ver los commits
-
+git log graph: ver los commits mas ordenados
+git diff: muestra los cambios en el fichero
+git checkout: volver a un fichero anterior
 
 */
