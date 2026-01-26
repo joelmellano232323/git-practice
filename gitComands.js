e328e097988fb56f81afde517769d04fc7c81c3e
Joe2323 <joelmellano232323@gmail.com>
2026-01-26 07:23:14 +0100

    Login añadido

--- gitComands.js
+++ gitComands.js
@@ -23,17 +23,26 @@ git config --global user.name "Joel2323": setear un nombre de usuario a nivel gl
 git config --global user.email "joel2323@gmail.com": setear un email a nivel global
 git init: iniciar git en el directorio actual
 git branch -m "cambiar rol": cambiar el nombre de master por otra cosa
+git alias.nombreDelAlias "comandos a ejecutar": crea un alias y le mete diversos comandos para ejecutar a la vez
 
 git status: muestra lo que falta commitear
-git add "nombre del fichero": prepararlo para commitear
-git commit -m "nombre del fichero": commmitearlo
+git add nombreDelFichero: prepararlo para commitear
+git add .: añade todo lo que hay en el fichero
+git commit -m nombreDelFichero: commmitearlo
 git log: ver los commits
 git log graph: ver los commits mas ordenados
+git reflog: historial de cambios
 git diff: muestra los cambios en el fichero
 git checkout: volver a un fichero anterior
-git checkout "numero del fichero": me muevo al fichero deseado
+git checkout numeroDelFichero: me muevo al fichero deseado
 git checkout main: volver al final
 git diff: me dice las diferencias del fichero actual y el ultimo guardado
 
+git reset --hard "IDdelCommit: voy a un commit, lo hago head y borro todo lo posterior
+git tag "nombre del tag": ponerle un tag a un commit
+git checkout tags/nombreDelTag: moverme a un tag
+
+git branch nombreDelaRama: creo una nueva rama desde la posicion actual
+git switch nombreDelaRama: me muevo de rama
 
 */
